d8af457312be1ab959cd4698efbbb0f14d6d19b8
Jon Tansey <jontansey@users.noreply.github.com>
2019-11-28 23:29:39 +0000

    Update to loan todo

---
+++ precache-manifest.2e968b0b35b25fadd243daff0870275f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43ad432dfb5752dad92cd3546ad612b",
+    "url": "/LoanToDo/index.html"
+  },
+  {
+    "revision": "deaa47f2b893464ced99",
+    "url": "/LoanToDo/static/js/2.1e324f18.chunk.js"
+  },
+  {
+    "revision": "9c9a140f8536a93cf730",
+    "url": "/LoanToDo/static/js/main.e66217f0.chunk.js"
+  },
+  {
+    "revision": "0df6bd2263053d34d568",
+    "url": "/LoanToDo/static/js/runtime-main.8be51873.js"
+  }
+]);
